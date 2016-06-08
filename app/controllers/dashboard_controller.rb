a220b3bbd595b132b1b39d794f7adfe6f9cd0db4
David Genord II <david@collectiveidea.com>
2016-06-07 21:58:33 -0400

    Try to prevent mobile safari caching

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,5 +2,6 @@ class DashboardController < ApplicationController
   def index
     @set_temp = SetTemp.first || SetTemp.new
     @reading = Reading.order(created_at: :desc).first
+    expires_now
   end
 end
